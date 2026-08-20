69b950b81e734782c81391dbea0de16e86c24d3b
mwaura-mwangi <fmwauramwangi@gmail.com>
2026-08-20 16:26:06 +0300

    update querry aggregations

--- sql_queries.sql
+++ sql_queries.sql
@@ -1,4 +1,15 @@
-SELECT coffee_name
+-- create or clone table from existing one
+CREATE TABLE coffee_sales_parkroad_two AS
+(
+ SELECT * FROM coffee_sales_parkroad
+);
 
-FROM coffee_sales_parkroad
+-- cofee that costs less the 30dollars
+SELECT *
+FROM coffee_sales_parkroad as c
+WHERE money <= 30.00
+LIMIT 10;
 
+
+SELECT *
+FROM coffee_sales_parkroad_two
